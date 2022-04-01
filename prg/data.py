eef9217390e790eb684f56fa067e376070d54818
SimonStaehli <simon.staehli@students.fhnw.ch>
2022-04-01 14:18:09 +0200

    Add old chnages

--- prg/data.py
+++ prg/data.py
@@ -4,16 +4,7 @@ from torch.utils.data import DataLoader, Dataset
 import torchvision.transforms as transforms
 import PIL
 import os
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
 import numpy as np
-=======
->>>>>>> 47d48b6526b303a6acb21ca69d041332a4bf51f8
-=======
->>>>>>> 47d48b6526b303a6acb21ca69d041332a4bf51f8
-=======
->>>>>>> 47d48b6526b303a6acb21ca69d041332a4bf51f8
 
 class CustomDataset(Dataset):
     """
@@ -21,19 +12,7 @@ class CustomDataset(Dataset):
     did not return correct amount of batch size.
     """
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
     def __init__(self, root_dir: str, transform: 'Compose' = None, dataset_fraction: float = 1):
-=======
-    def __init__(self, root_dir: str, transform: 'Compose' = None):
->>>>>>> 47d48b6526b303a6acb21ca69d041332a4bf51f8
-=======
-    def __init__(self, root_dir: str, transform: 'Compose' = None):
->>>>>>> 47d48b6526b303a6acb21ca69d041332a4bf51f8
-=======
-    def __init__(self, root_dir: str, transform: 'Compose' = None):
->>>>>>> 47d48b6526b303a6acb21ca69d041332a4bf51f8
         """
         
         Params:
@@ -46,9 +25,6 @@ class CustomDataset(Dataset):
             
         all_images: list
             List of all images names in the root dir.
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
         
         dataset_fraction: int = 1
             fraction of dataset to take from for training. default = 1
@@ -71,43 +47,12 @@ class CustomDataset(Dataset):
         
     def __len__(self):
         return len(self.images)
-=======
-=======
->>>>>>> 47d48b6526b303a6acb21ca69d041332a4bf51f8
-=======
->>>>>>> 47d48b6526b303a6acb21ca69d041332a4bf51f8
-        """
-        self.root_dir = root_dir
-        self.transform = transform
-        self.all_images = [img for img in os.listdir(self.root_dir) if '.jpg' in img]
-
-    def __len__(self):
-        return len(os.listdir(self.root_dir))
-<<<<<<< HEAD
-<<<<<<< HEAD
->>>>>>> 47d48b6526b303a6acb21ca69d041332a4bf51f8
-=======
->>>>>>> 47d48b6526b303a6acb21ca69d041332a4bf51f8
-=======
->>>>>>> 47d48b6526b303a6acb21ca69d041332a4bf51f8
 
     def __getitem__(self, idx):
         if torch.is_tensor(idx):
             idx = idx.tolist()
         
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
         img_name = os.path.join(self.root_dir, self.images[idx])
-=======
-        img_name = os.path.join(self.root_dir, self.all_images[idx])
->>>>>>> 47d48b6526b303a6acb21ca69d041332a4bf51f8
-=======
-        img_name = os.path.join(self.root_dir, self.all_images[idx])
->>>>>>> 47d48b6526b303a6acb21ca69d041332a4bf51f8
-=======
-        img_name = os.path.join(self.root_dir, self.all_images[idx])
->>>>>>> 47d48b6526b303a6acb21ca69d041332a4bf51f8
         image = PIL.Image.open(img_name)
 
         if self.transform:
@@ -116,19 +61,7 @@ class CustomDataset(Dataset):
         return image
     
 def get_dataloader(root_dir: str, transforms: 'torch.utils.Compose', 
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
                    batch_size: int, workers: int, dataset_fraction: float = 1):
-=======
-                   batch_size: int, workers: int):
->>>>>>> 47d48b6526b303a6acb21ca69d041332a4bf51f8
-=======
-                   batch_size: int, workers: int):
->>>>>>> 47d48b6526b303a6acb21ca69d041332a4bf51f8
-=======
-                   batch_size: int, workers: int):
->>>>>>> 47d48b6526b303a6acb21ca69d041332a4bf51f8
     """
     Functino returns a dataloader with given parameters
     
@@ -147,19 +80,7 @@ def get_dataloader(root_dir: str, transforms: 'torch.utils.Compose',
         Amount of CPU workers for the loading of data into gpu.
     """
     
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
     custom_dataset = CustomDataset(root_dir=root_dir, transform=transforms, dataset_fraction=dataset_fraction)
-=======
-    custom_dataset = CustomDataset(root_dir=root_dir, transform=transforms)
->>>>>>> 47d48b6526b303a6acb21ca69d041332a4bf51f8
-=======
-    custom_dataset = CustomDataset(root_dir=root_dir, transform=transforms)
->>>>>>> 47d48b6526b303a6acb21ca69d041332a4bf51f8
-=======
-    custom_dataset = CustomDataset(root_dir=root_dir, transform=transforms)
->>>>>>> 47d48b6526b303a6acb21ca69d041332a4bf51f8
     
     return DataLoader(dataset=custom_dataset, 
                       batch_size=batch_size, 
